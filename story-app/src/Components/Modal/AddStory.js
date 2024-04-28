28a4fa9630f59a0d5661847441463914b701055f
Reshaali35 <96935193+reshali35@users.noreply.github.com>
2024-04-28 11:56:50 +0000

    Add Slide and form override correct view

--- story-app/src/Components/Modal/AddStory.js
+++ story-app/src/Components/Modal/AddStory.js
@@ -1,60 +1,133 @@
 import "./Modal.css";
+import "./Slide.css";
 import "./SignIn.css";
 import React, { useState } from "react";
 import closeButton from "./closeIcon.jpg";
 
-function AddStory({ onClose }) {
+function Slide({ onClose }) {
+  const [slideList, setSlideList] = useState([{ slide: "" }]);
+  const [formList, setFormList] = useState([{ form: "" }]);
+
+  const handleSlideAdd = () => {
+    setSlideList([...slideList, { slide: "" }]);
+    setFormList([...formList, { form: "" }]);
+  };
+  const handleSlideRemove = (index) => {
+    const slist = [...slideList];
+    const flist = [...formList];
+    slist.splice(index, 1);
+    flist.splice(index, 1);
+    setSlideList(slist);
+    setFormList(flist);
+  };
+
+  const handlePreviousButton = (index) => {};
+  const handleNextButton = (index) => {};
+
   return (
     <>
       <div className="modal">
         <div onClick={onClose} className="overlay"></div>
         <div className="modal-content">
           <div>
-            <form>
-              <div className="formInput-container">
-                <div className="input-label">
-                  <p id="Heading">Heading:</p>
-                </div>
-                <div className="input-box">
-                  <input type="text" placeholder="Your Heading"></input>
-                </div>
+            <div className="AllSlideStep" id="slideStep">
+              <label htmlFor="slide" className=" Slide-Container">
+                {/* <div className="slide-box" id="slide-box-one">
+                  Slide 1
+                </div> */}
+                {slideList.map((singleSlide, index) => (
+                  <div key={index}>
+                    <div className="allslides">
+                      <div className="slide-box" id="slide-one">
+                        Slide {index + 1}
+                        {index > 0 && (
+                          <img
+                            src={closeButton}
+                            className="slide-close-button"
+                            onClick={() => handleSlideRemove(index)}
+                          />
+                        )}
+                      </div>
+                      {slideList.length - 1 === index &&
+                        slideList.length < 6 && (
+                          <div
+                            className="slide-box"
+                            id="add"
+                            onClick={handleSlideAdd}
+                          >
+                            Add +
+                          </div>
+                        )}
+                    </div>
+                    {/*  */}
+                  </div>
+                ))}
+              </label>
+              <label htmlFor="form" className="form-Container">
+                {formList.map((singleForm, index) => (
+                  <div key={index}>
+                    <form>
+                      <div className="formInput-container">
+                        <div className="input-label">
+                          <p id="Heading">Heading:</p>
+                        </div>
+                        <div className="input-box">
+                          <input type="text" placeholder="Your Heading"></input>
+                        </div>
 
-                <div className="input-label">
-                  <p id="description">Description:</p>{" "}
-                </div>
-                <div className="text-area">
-                  <textarea placeholder="Story Description"></textarea>
-                </div>
+                        <div className="input-label">
+                          <p id="description">Description:</p>
+                        </div>
+                        <div className="text-area">
+                          <textarea placeholder="Story Description"></textarea>
+                        </div>
 
-                <div className="input-label">
-                  <p id="storyImage">Image:</p>
-                </div>
-                <div className="input-box">
-                  <input type="text" placeholder="Add Image URL"></input>
-                </div>
+                        <div className="input-label">
+                          <p id="storyImage">Image:</p>
+                        </div>
+                        <div className="input-box">
+                          <input
+                            type="text"
+                            placeholder="Add Image URL"
+                          ></input>
+                        </div>
 
-                <div className="input-label">
-                  <p id="category">Category:</p>
-                </div>
-                <div>
-                  <select className="input-box">
-                    <option>Select Category</option>
-                    <option>Food</option>
-                    <option>Health and Fitness</option>
-                    <option>Travel</option>
-                    <option>Movies</option>
-                    <option>Education</option>
-                  </select>
-                </div>
-              </div>
-              <div className="buttonsInput-container">
-                <button className="previousButton">Previous</button>
-                <button className="nextButton">Next</button>
-                <button className="postButton" type="submit">
-                  post
-                </button>
-              </div>
-            </form>
+                        <div className="input-label">
+                          <p id="category">Category:</p>
+                        </div>
+                        <div>
+                          <select className="input-box">
+                            <option>Select Category</option>
+                            <option>Food</option>
+                            <option>Health and Fitness</option>
+                            <option>Travel</option>
+                            <option>Movies</option>
+                            <option>Education</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="buttonsInput-container">
+                        <button
+                          className="previousButton secondPrev"
+                          onClick={() => handlePreviousButton(index)}
+                        >
+                          Previous
+                        </button>
+                        <button
+                          className="nextButton"
+                          onClick={() => handleNextButton(index)}
+                        >
+                          Next
+                        </button>
+                        <button className="postButton" type="submit">
+                          post
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                ))}
+              </label>
+            </div>
           </div>
           <img src={closeButton} className="close-modal" onClick={onClose} />
         </div>
@@ -63,4 +136,4 @@ function AddStory({ onClose }) {
   );
 }
 
-export default AddStory;
+export default Slide;
